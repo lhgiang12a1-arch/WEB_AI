37857e7e4f85d954a58b78deb18dba44788544e3
lhgiang12a1 <lhgiang12a1@gmail.com>
2026-07-10 17:39:19 +0700

    hghg

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Loader script that attaches the original compiled script from assets.
-(function(){
-  const s = document.createElement('script');
+(function () {
+  const s = document.createElement("script");
   s.defer = true;
-  s.crossOrigin = 'anonymous';
-  s.src = './Flames App_files/v4513226cdae34746b4dedf0b4dfa099e1781791509496';
+  s.crossOrigin = "anonymous";
+  s.src = "./Flames App_files/v4513226cdae34746b4dedf0b4dfa099e1781791509496";
   document.head.appendChild(s);
 })();
